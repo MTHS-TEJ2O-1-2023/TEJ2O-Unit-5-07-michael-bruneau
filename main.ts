8be5ec05a3ca2293973855483f540bcb57a7838a
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-11 10:52:22 -0500

    setup

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael Bruneau
+ * Created on: Nov 2023
+ * This program changes 
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
