77e9f467621576eaef00e758ba141b2f03d7240f
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-11 11:53:07 -0500

    mark

--- main.ts
+++ main.ts
@@ -5,6 +5,23 @@
  * This program changes 
 */
 
+// variables
+const servoNumber1 = robotbit.Servos.S1
+const degreeSign: basic.showLeds(`
+  # # # . .
+  # . # . .
+  # # # . .
+  . . . . .
+  . . . . .
+`)
+
 // setup
-basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servoNumber1, 0)
+  basic.showString(('0') + degreeSign)
+  basic.pause(1000)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
